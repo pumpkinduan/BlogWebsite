be4e41e866784c05a7f065c9db1aea402d3465d5
pumpkin <pumpkin_1208@163.com>
2020-10-06 22:47:52 +0800

    添加字段说明

--- back-end/src/dto/comment.dto.ts
+++ back-end/src/dto/comment.dto.ts
@@ -1,14 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { userDto } from './user.dto';
 export namespace commentDto {
-    export interface basicCommentDto {
+    export class basicCommentDto {
         id: number;
         content: string;
         created_at: string;
         related_user: userDto.basicUserDto; // 留言关联的用户，与用户为一对一关系
         children: basicCommentDto[] // 回复
     }
-    export interface createCommentDto {
+    export class createCommentDto {
+        @ApiProperty({ description: '留言内容' })
         content: string;
-        related_user_id: number;
+        @ApiProperty({ description: '留言的用户' })
+        related_user: userDto.basicUserDto;
     }
 }
--- back-end/src/dto/user.dto.ts
+++ back-end/src/dto/user.dto.ts
@@ -1,26 +1,65 @@
+import { ApiProperty, PickType } from "@nestjs/swagger";
 // 管理员DTO
 type notice = { notice: string };
 export namespace userDto {
-    export type superUserProfile = Record<
-        'nickname' | 'email' | 'github' | 'avatar' | 'brief',
-        string
-    >;
-    export interface superUserDto {
+    export class superUserProfile {
+        @ApiProperty({ description: '管理员的昵称' })
+        nickname: string;
+        @ApiProperty({ description: '邮箱' })
+        email: string;
+        @ApiProperty({ description: 'GitHub账号' })
+        github: string;
+        @ApiProperty({ description: '头像地址' })
+        avatar: string;
+        @ApiProperty({ description: '关于我，HTML格式的字符串' })
+        brief: string;
+    }
+    export class superUserDto {
         profiles: superUserProfile & notice;
         moment_ids: number[]; // 文章id
         word_ids: number[]; // 给我的留言id
     }
-    export interface basicUserDto
+    export class basicUserDto
+        extends PickType(superUserProfile, ['nickname', 'email', 'avatar']) {
+        id: number;
+        comment_ids: number[]; // 用户关联的留言，与留言为一对多关系
+        related_post_ids: number[]; // 用户关联的文章，与文章为多对多关系
+    }
+    export class createUserDto
+        extends PickType(superUserProfile, ['nickname', 'email']) {
+        @ApiProperty({ description: '密码' })
+        password: string;
+    }
+    export class createSuperUserDto {
+        @ApiProperty({ description: '管理员的昵称' })
+        nickname: string;
+        @ApiProperty({ description: '密码' })
+        password: string;
+    }
+}
+
+export namespace userInterface {
+    export type superUserProfile = Record<
+        'nickname' | 'email' | 'github' | 'avatar' | 'brief',
+        string
+    >;
+    export interface superUserInterface {
+        profiles: superUserProfile & notice;
+        moment_ids: number[]; // 文章id
+        word_ids: number[]; // 给我的留言id
+    }
+    export interface basicUserInterface
         extends Pick<superUserProfile, 'nickname' | 'email' | 'avatar'> {
         id: number;
         comment_ids: number[]; // 用户关联的留言，与留言为一对多关系
         related_post_ids: number[]; // 用户关联的文章，与文章为多对多关系
     }
-    export interface createUserDto
+    export interface createUserInterface
         extends Pick<superUserProfile, 'nickname' | 'email'> {
         password: string;
     }
-    export interface createSuperUserDto {
+    export interface createSuperUserInterface {
+
         username: string;
         password: string;
     }
