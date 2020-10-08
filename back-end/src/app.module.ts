28b594c3ebedf56d10f6d1c2734b56fc0f595fa6
pumpkin <pumpkin_1208@163.com>
2020-10-08 11:06:05 +0800

    新增module目录，用于存放各个模块

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
-import { CommentModule } from './comment/comment.module';
-import { UserModule } from './user/user.module';
+import { PostModule } from 'module/post/post.module';
+import { CommentModule } from 'module/comment/comment.module';
+import { UserModule } from 'module/user/user.module';
 
 @Module({
   imports: [PostModule, CommentModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
