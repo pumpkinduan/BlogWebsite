902d79d0bca1a8b297b0806631f9074d63c9f9b4
pumpkin <pumpkin_1208@163.com>
2020-10-10 21:28:08 +0800

    定义成功返回的消息提示的枚举类型

--- back-end/src/common/interfaces/result.interface.ts
+++ back-end/src/common/interfaces/result.interface.ts
@@ -5,3 +5,21 @@ export interface ResultInterface {
     success: boolean;
     data?: any;
 }
+export namespace SuccessMessage {
+    export enum Post {
+        CREATE = '成功创建帖子',
+        DELETE = '成功删除帖子',
+        UPDATE = '成功更新帖子',
+        OK = 'OK'
+    }
+    export enum Comment {
+        CREATE = '成功创建留言',
+        DELETE = '成功删除留言',
+        OK = 'OK'
+    }
+    export enum User {
+        CREATE = '成功创建用户',
+        DELETE = '成功删除用户',
+        OK = 'OK'
+    }
+}
