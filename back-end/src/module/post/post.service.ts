1d4a8fb0ec14d27d3af0178b8f0d9d3d5b874bc7
pumpkin <pumpkin_1208@163.com>
2020-10-11 20:40:39 +0800

    指定分页查询返回帖子的字段

--- back-end/src/module/post/post.service.ts
+++ back-end/src/module/post/post.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { Post } from 'entity/post.entity'
 import { Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
+import { PostInterface } from 'common/interfaces/index.interface'
 @Injectable()
 export class PostService {
     constructor(@InjectRepository(Post) readonly postRepository: Repository<Post>) { }
     async findAndCount(page: number, pageSize: number): Promise<[Post[], number]> {
         // 分页
         const offset = page * pageSize - pageSize;
-        return await this.postRepository.findAndCount({ skip: offset, take: pageSize });
+        return await this.postRepository.findAndCount({ skip: offset, take: pageSize, select: ['id', 'createdAt', 'title', 'visitors', 'downloads', 'coverUrl', 'likes', 'comments'] });
     }
 
-    async create(createPost: Post): Promise<void> {
+    async create(createPost: PostInterface.CreatePost): Promise<void> {
         await this.postRepository.save(createPost);
     }
 
