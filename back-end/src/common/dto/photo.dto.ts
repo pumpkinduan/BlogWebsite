ed28218a3603ed96d3e2169a987e8dfe14e959ae
duanzz <34852848+pumpkinduan@users.noreply.github.com>
2021-01-13 13:09:43 +0800

    photo类型定义

--- back-end/src/common/dto/photo.dto.ts
+++ back-end/src/common/dto/photo.dto.ts
@@ -1,17 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum } from "class-validator";
+import { IsEnum, IsNotEmpty } from "class-validator";
 
 export enum PHOTO_TYPE {
-    POST, // 文章的图片
-    WALL// 相册墙
+    POST = 'POST', // 文章的图片
+    WALL = 'WALL'// 相册墙
 }
 export class CreatePhotoDto {
-    @ApiProperty({ description: '图片类型', enum: PHOTO_TYPE })
+    @ApiProperty({ description: '图片类型', enum: PHOTO_TYPE, example: PHOTO_TYPE.POST })
     @IsEnum(PHOTO_TYPE)
     type: PHOTO_TYPE;
 }
 
 export class DeletePhotoDto {
+    @IsNotEmpty()
     @ApiProperty({ description: '图片id' })
     id: number
 }
--- back-end/src/common/interfaces/photo.interface.ts
+++ back-end/src/common/interfaces/photo.interface.ts
@@ -1,4 +1,9 @@
 export namespace PhotoInterface {
+
+    export enum PHOTO_TYPE {
+        POST = 'POST', // 文章的图片
+        WALL = 'WALL'// 相册墙
+    }
     export interface BasicPhoto {
         id: string;
         path: string;
